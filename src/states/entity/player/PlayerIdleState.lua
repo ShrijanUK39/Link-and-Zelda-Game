7e0392f1e519bd0207fc3dc63c52209f62837f31
SHRIJAN GTA <shrijangamer32@gmail.com>
2022-02-11 11:36:04 +0000

    Link and Zelda Game

---
+++ src/states/entity/player/PlayerIdleState.lua
@@ -0,0 +1,35 @@
+PlayerIdleState = Class{__includes = EntityIdleState}
+
+function PlayerIdleState:enter(params)
+    self.entity.offsetY = 5
+    self.entity.offsetX = 0
+    self.entity.idle = true
+end
+
+function PlayerIdleState:update(dt)
+    EntityIdleState.update(self, dt)
+end
+
+function PlayerIdleState:update(dt)
+    if love.keyboard.isDown(PLAYER_LEFT) or love.keyboard.isDown(PLAYER_RIGHT) or
+       love.keyboard.isDown(PLAYER_UP) or love.keyboard.isDown(PLAYER_DOWN) then
+        self.entity:changeState('walk')
+    end
+end
+
+function PlayerIdleState:action(dt)
+    
+    if self.entity.item == nil then
+        self.entity:attemptLift(dt)
+       
+        if self.entity.item == nil then
+            self.entity:changeState('swing-sword')
+        
+        else
+            self.entity:changeState('lift')
+        end
+    
+    else
+        self.entity:changeState('throw')
+    end
+end
